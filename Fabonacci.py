8d926aa1c7c2b6affd47bef2b6df929b71f9e36b
darcy <suan24@qq.com>
2017-11-13 15:07:55 +0800

    update some python files

---
+++ Fabonacci.py
@@ -0,0 +1,24 @@
+#输入一个大于等于3的值n，输出斐波纳契数列的前n项。
+# 注：斐波纳契数列：1,1,2,3,5,8,13,21...前两项为1，从第3项起，每一项是前两项的和
+
+'''print('please enter  a number:')
+num = int(input())
+i = 0
+sum = 0
+res = []
+
+if i == 0:
+    for i in range(1,2):
+        i += 1
+        sum = 1
+        res = sum
+        print(res)
+if i >= 2:
+    for i in range(1,num):
+        res = (i-1) + (i-2)
+        i += 1
+        print(res)
+'''
+#  我想的太复杂了
+num = print('please enter a number:')
+n = num
---
+++ forIn_1108.py
@@ -0,0 +1,39 @@
+#_*_ coding: utf-8 _*
+#1：改写1+2+3....+100
+
+# res = 0
+# for i in range(1,101):
+#     res += i
+# print(res)
+
+
+#:2：输入一个值，输出以这个值为公比，1为首项的等比数列前10项
+
+# print("please enter a number:")
+# num = int(input())  #2
+# a = 1
+# i = 0
+# res = 1
+# print(1)
+# for i in range(1,10):
+#     res *= num
+#     i += 1
+#     print(res)
+
+
+#2的优化方案
+
+# print("please enter a number:")
+# q = int(input())  #2
+# a = 1
+# i = 0
+# res = 1
+#
+# for i in range(1,10):
+#     print(res)
+#     res *= q
+#     i += 1
+
+
+
+
