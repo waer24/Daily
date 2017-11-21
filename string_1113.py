798218f6e4c0ed8b2d17a49e28f30db73dacbd20
darcy <suan24@qq.com>
2017-11-21 20:27:59 +0800

    learned list by simply

--- string_1113.py
+++ string_1113.py
@@ -61,3 +61,4 @@ a = ['a','b','c']
 s = ','
 b = s.join(a)
 print(b)
+
---
+++ string_index.py
@@ -0,0 +1,12 @@
+word = 'helloward'
+# for i in word:
+#     print (i)
+
+print (word[2])
+print(word[2:5])
+
+f = open('files/var_1114.html')
+data = f.read()
+print(data)
+data.close('files/var_1114.html')
+
